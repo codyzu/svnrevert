6e085ade4b8298d8ec115d0b2d20c2419b21a7b0
cody <cody.zuschlag@gmail.com>
2014-11-11 22:55:17 +0100

    started parsing status xml

--- sgit.py
+++ sgit.py
@@ -2,14 +2,32 @@ __author__ = 'cody'
 
 import click
 import svn.local
+import xml.etree.ElementTree as ET
 
+def iteritems():
+    tree = ET.parse('svnstatus.xml')
+    for entry in tree.iter('entry'):
+        path = entry.attrib['path']
+        status = entry.find('wc-status')
+        item = status.attrib['item']
+        yield path, item
 
 @click.command()
 @click.argument("path", default=".")
 def revert(path):
-    r = svn.local.LocalClient(path)
-    i = r.info()
+    #r = svn.local.LocalClient(path)
+    #i = r.info()
     x = 5
+
+    items = list(iteritems())
+
+
+    unversioned = [(path, item) for (path, item) in items if item == 'unversioned']
+    externals = [(path, item) for (path, item) in items if item == 'external']
+    modified = [(path, item) for (path, item) in items if item == 'modified']
+
+    for path, item in iteritems():
+        print("{0}: {1}".format(path, item))
 
 
 if __name__ == '__main__':
