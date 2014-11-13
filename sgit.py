83c39506792b86e903aa95d0c171f1df6530c4cf
codyzu <cody.zuschlag@gmail.com>
2014-11-13 13:22:29 +0100

    more refactoring for clarity

--- sgit.py
+++ sgit.py
@@ -1,27 +1,29 @@
-from pathlib import Path
-from collections import namedtuple
+import pathlib
+import xml.etree.ElementTree
+
+import collections
 import click
-from operator import attrgetter
+import operator
 import shutil
 import os
 import svn.local
-import xml.etree.ElementTree as ET
+
 
 __author__ = 'cody'
 
-
-SvnItem = namedtuple('SvnItem', ['path', 'item'])
+SvnItem = collections.namedtuple('SvnItem', ['path', 'item'])
 dryrun = False
 repo = None
 workingdir = ""
 
 
 def get_svn_statuses():
+    """Execute svn status and parse the results for the status of all items"""
     click.echo("Getting svn status for: " + workingdir)
     xmlstatus = repo.run_command('status', ['--xml', workingdir], combine=True)
 
     click.echo("Parsing SVN results")
-    tree = ET.fromstring(xmlstatus)
+    tree = xml.etree.ElementTree.fromstring(xmlstatus)
 
     svnitems = list()
     for entry in tree.iter('entry'):
@@ -34,6 +36,7 @@ def get_svn_statuses():
 
 
 def revert_dirs_recursively(paths):
+    """svn revert all list of directories and print the results"""
     result = ""
     with click.progressbar(paths) as revert_paths:
         for path in revert_paths:
@@ -46,9 +49,10 @@ def revert_dirs_recursively(paths):
         click.secho("No changes made in dry-run mode", fg='green')
 
 
-def delete_items(items):
-    for item in items:
-        p = Path(item)
+def delete_items(paths):
+    """Delete a list of paths to files or directories"""
+    for path in paths:
+        p = pathlib.Path(path)
         normal_path = str(p)  # prefer the normalized path from pathlib
         click.secho("Removing: " + normal_path, fg='red')
         if p.is_dir():
@@ -60,6 +64,47 @@ def delete_items(items):
 
     if dryrun:
         click.secho("No changes made in dry-run mode", fg='green')
+
+
+def summarize_changes(svnitems):
+    """List all of the changes and return true if any changes are found"""
+    changes = sorted([svnitem for svnitem in svnitems if svnitem.item != 'external'], key=operator.attrgetter('path'))
+    for svnitem in changes:
+        click.secho("{0}: {1}".format(svnitem.item, svnitem.path), fg='yellow')
+
+    click.echo("Found {0} changes to revert".format(len(changes)))
+
+    return len(changes) > 0
+
+
+def revert_changes(svnitems):
+    """Revert the working dir and all externals"""
+    revert_dirs = [svnitem.path for svnitem in svnitems if svnitem.item == 'external']
+    revert_dirs.insert(0, workingdir)  # don't forget to revert the working dir, since it is not an external
+    revert_dirs = sorted(revert_dirs)
+
+    click.echo("The following directories will be recursively reverted:")
+
+    for revert_dir in revert_dirs:
+        click.secho(revert_dir, fg='yellow')
+
+    if click.confirm("Recursively revert the above {0} directories".format(len(revert_dirs)), abort=True):
+        click.secho("Reverting...", fg='red')
+        revert_dirs_recursively(revert_dirs)
+
+
+def delete_unversioned(svnitems):
+    """Delete all items (files/dirs) are that are unversioned"""
+    unversioned = sorted([svnitem.path for svnitem in svnitems if svnitem.item == 'unversioned'])
+    click.echo("The following items are unversioned:")
+
+    for item in unversioned:
+        click.secho(item, fg='yellow')
+
+    if (len(unversioned) > 0 and
+            click.confirm("Delete the above {0} files/directories".format(len(unversioned)), abort=True)):
+        click.secho("Deleting...", fg='red')
+        delete_items(unversioned)
 
 
 @click.command()
@@ -75,49 +120,20 @@ def revert(dry_run, path):
     repo = svn.local.LocalClient(path)
     workingdir = path
 
-    # svn status
+    # get the svn status
     svnitems = get_svn_statuses()
 
-    # collect all of the changes, print them, and exit if there are none
-    changes = sorted([svnitem for svnitem in svnitems if svnitem.item != 'external'], key=attrgetter('path'))
-    for svnitem in changes:
-        click.secho("{0}: {1}".format(svnitem.item, svnitem.path), fg='yellow')
-
-    click.echo("Found {0} changes to revert".format(len(changes)))
-
-    if len(changes) == 0:
+    # if there are no changes, exit
+    if not summarize_changes(svnitems):
         click.secho("Exiting", fg='green')
         return
 
-    # collect the dirs we will revert, print them, and then revert
-    # for simplicity, we simply revert all externals and the working dir
-    revert_dirs = [svnitem.path for svnitem in svnitems if svnitem.item == 'external']
-    revert_dirs.insert(0, workingdir)  # don't forget to revert the working dir, since it is not an external
-    revert_dirs = sorted(revert_dirs)
+    revert_changes(svnitems)
 
-    click.echo("The following directories will be recursively reverted:")
-
-    for revert_dir in revert_dirs:
-        click.secho(revert_dir, fg='yellow')
-
-    if click.confirm("Recursively revert the above {0} directories".format(len(revert_dirs)), abort=True):
-        click.secho("Reverting...", fg='red')
-        revert_dirs_recursively(revert_dirs)
-
-    # new svn status after the above revert
+    # refresh svn status after reverting
     svnitems = get_svn_statuses()
 
-    # collect all unversioned files/dirs, print them, and then delete them
-    unversioned = sorted([svnitem.path for svnitem in svnitems if svnitem.item == 'unversioned'])
-    click.echo("The following items are unversioned:")
-
-    for item in unversioned:
-        click.secho(item, fg='yellow')
-
-    if (len(unversioned) > 0 and
-            click.confirm("Delete the above {0} files/directories".format(len(unversioned)), abort=True)):
-        click.secho("Deleting...", fg='red')
-        delete_items(unversioned)
+    delete_unversioned(svnitems)
 
 
 if __name__ == '__main__':
