3844149fffd5dde794769257258d55f1003d578a
codyzu <cody.zuschlag@gmail.com>
2015-07-24 13:16:02 +0200

    renamed to svnrevert

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
-    py_modules=['sgit'],
+    py_modules=['svnrevert'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -106,7 +106,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'svnrevert=sgit:revert',
+            'svnrevert=svnrevert:revert',
         ],
     },
 )
